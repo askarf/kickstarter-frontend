26e6a2d5d300c8a319f0d57366ff26dbbc8c4337
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-21 06:23:32 +1000

    content created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Content } from "./content";
+
 function App() {
   return (
     <div>
       <Header />
-      <h1>Test</h1>
+      <Content />
       <Footer />
     </div>
   );
---
+++ src/Content.jsx
@@ -0,0 +1,8 @@
+export function Content() {
+  return (
+    <div>
+      <h1>Content</h1>
+      <h2>Content 2</h2>
+    </div>
+  );
+}

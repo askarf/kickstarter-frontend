a1b2deee7c82b4780528844c637e6c0cddbcf35d
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-22 05:17:16 +1000

    added header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,48 +3,34 @@ export function Header() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          StartKicker
+          Start Kicker
         </a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                All Projects
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                My Pledges
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-disabled="true">
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-disabled="true">
-                Logout
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">
+              All Projects
+            </a>
+            <a className="nav-link active" href="#">
+              My Pledges
+            </a>
+            <a className="nav-link active " href="#">
+              Login
+            </a>
+            <a className="nav-link active" aria-disabled="true">
+              Logout
+            </a>
+          </div>
         </div>
       </div>
     </nav>

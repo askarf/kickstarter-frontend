5df827e01d1deef933064035f2f3347dd955b9ce
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-22 05:03:28 +1000

    actively editing header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,50 @@
 export function Header() {
   return (
-    <header>
-      <h1>Header Goes Here</h1>
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">StartKicker</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                All Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                My Pledges
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-disabled="true">
+                Logout
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }

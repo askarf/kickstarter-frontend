c50994d1f58e4ac7f94c0c086b55bcabd628f671
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-23 05:53:12 +1000

    "signup"

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,12 +1,36 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    console.log(handleSubmit, params);
+    axios.post("http://localhost:3000.users.json", params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    });
+  };
   return (
     <div>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Name:
           <input name="name" type="text" />
         </div>
+        <div>
+          Email:
+          <input name="email" type="email" />
+        </div>
+        <div>
+          Password:
+          <input name="password" type="password" />
+        </div>
+        <div>
+          Password Confirmation:
+          <input name="password_confirmation" type="password" />
+        </div>
+        <button className="btn btn-dark">Sign Up</button>
       </form>
     </div>
   );

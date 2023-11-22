58cb1ac13d0f4715d0cd900690a3810bd375b232
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-23 04:44:07 +1000

    signup page

---
+++ src/signup.jsx
@@ -0,0 +1,13 @@
+export function Signup() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <div>
+          Name:
+          <input name="name" type="text" />
+        </div>
+      </form>
+    </div>
+  );
+}
